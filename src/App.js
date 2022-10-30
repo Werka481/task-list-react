a6ad70886032a7bc8eb649cd8c58c9d1dc5bb072
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2022-10-30 12:33:56 +0100

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -28,6 +28,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id))
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+      return task;
+    }))
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -37,7 +46,13 @@ function App() {
       />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />}
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />}
         extraContent={
           <Buttons
             tasks={tasks}
