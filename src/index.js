19a412e5ba136b3a0da1f26260ac9798497b16e8
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2022-12-12 19:55:42 +0100

    added GlobalStyles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
 import App from './App';
+import GlobalStyles from './globalStyles';
 import reportWebVitals from './reportWebVitals';
 import store from "./store";
 
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyles/>
       <App />
     </Provider>
   </React.StrictMode>
