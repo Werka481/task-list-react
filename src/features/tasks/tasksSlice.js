4c0d82294305f62bb07f429811c32ffc4bfc95cd
Weronika Lopato-Jedrasik <weronika481@wp.pl>
2022-12-13 12:33:53 +0100

    added selectTaskByQuery select

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -57,6 +57,17 @@ export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectAllTaskDone = state => selectTasks(state).every(({ done }) => done);
 export const selectLoadingStatus = state => selectTasksState(state).loading;
 
-export const getTaskById = (state, taskId) => selectTasks(state).find(({id}) => id === taskId);
+export const getTaskById = (state, taskId) => selectTasks(state).find(({ id }) => id === taskId);
+
+export const selectTaskByQuery = (state, query) => {
+    const tasks = selectTasks(state);
+
+    if (!query || query.trim() === "") {
+        return tasks;
+    }
+
+    return tasks.filter(({ content }) =>
+        content.toUpperCase().includes(query.trim().toUpperCase()));
+};
 
 export default tasksSlice.reducer;
